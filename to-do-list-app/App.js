17481c52459e89a02bc8d7b0419b237274e7fb20
fullyalive <wogud1202@gmail.com>
2018-06-27 19:08:12 +0900

    Coding the UI
    adding shadow effect

--- to-do-list-app/App.js
+++ to-do-list-app/App.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
@@ -8,7 +18,7 @@ export default class App extends React.Component {
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>To Do List</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placholder={"New To Do"} />
+          <TextInput style={styles.input} placeholder={"New To Do"} />
         </View>
       </View>
     );
@@ -25,6 +35,29 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 30,
     marginTop: 50,
-    fontWeight: "300"
-  }
+    fontWeight: "300",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          widht: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {}
 });
