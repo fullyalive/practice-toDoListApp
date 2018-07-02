34eb06a8b99f5393de4a6d1d986e5b4d7ef8b907
fullyalive <wogud1202@gmail.com>
2018-07-02 22:22:29 +0900

    Updating a To Do

--- to-do-list-app/ToDo.js
+++ to-do-list-app/ToDo.js
@@ -23,7 +23,8 @@ export default class ToDo extends Component {
     deleteToDo: PropTypes.func.isRequired,
     id: PropTypes.string.isRequired,
     uncompleteToDo: PropTypes.func.isRequired,
-    completeToDo: PropTypes.func.isRequired
+    completeToDo: PropTypes.func.isRequired,
+    updateToDo: PropTypes.func.isRequired
   };
 
   render() {
@@ -118,6 +119,9 @@ export default class ToDo extends Component {
     });
   };
   _finishEditing = () => {
+    const { toDoValue } = this.state;
+    const { id, updateToDo } = this.props;
+    updateToDo(id, toDoValue);
     this.setState({
       isEditing: false
     });
